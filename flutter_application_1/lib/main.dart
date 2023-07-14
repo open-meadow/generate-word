43cea413485f00672ddf76cae6e921c136854684
open-meadow <rohan.varughese@protonmail.com>
2023-07-13 23:37:25 -0400

    Implement official solution

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -153,11 +153,24 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var favorites = appState.favorites;
 
-    return Column(
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favourites yet'),
+      );
+    }
+
+    return ListView(
       children: [
-        for (var f in favorites) BigCard(pair: f),
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ' '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
       ],
     );
   }
